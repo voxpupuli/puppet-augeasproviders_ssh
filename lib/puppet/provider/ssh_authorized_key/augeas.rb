645299d9e75020f698509090ef500164485a021c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-08-26 17:10:37 +0200

    Add ssh_authorized_key provider

---
+++ lib/puppet/provider/ssh_authorized_key/augeas.rb
@@ -0,0 +1,101 @@
+# Alternative Augeas-based providers for Puppet
+#
+# Copyright (c) 2014 Raphaël Pinson
+# Licensed under the Apache License, Version 2.0
+
+Puppet::Type.type(:ssh_authorized_key).provide(:augeas, :parent => Puppet::Type.type(:augeasprovider).provider(:default)) do
+  desc "Uses Augeas API to update ssh authorized keys"
+
+  default_file { '' }
+
+  lens { 'Authorized_Keys.lns' }
+
+  confine :feature => :augeas
+  defaultfor :feature => :augeas
+
+  resource_path do |resource|
+    "$target/*[comment='#{resource[:name]}']"
+  end
+
+  def self.instances
+    augopen do |aug|
+      aug.match('$target/key').map do |key|
+        options = aug.match("#{key}/options/*").map do |opt|
+          label = path_label(aug, opt)
+          value = aug.get(opt)
+          if value.nil?
+            label
+          else
+            "#{label}=\"#{value}\""
+          end
+        end
+
+        uid = Puppet::FileSystem.stat(target).uid
+        user = Etc.getpwuid(uid).name
+
+        new({
+          :ensure  => :present,
+          :name    => aug.get("#{key}/comment"),
+          :key     => aug.get(key),
+          :type    => aug.get("#{key}/type"),
+          :options => options,
+          :user    => user
+        })
+      end
+    end
+  end
+
+  def self.set_options(aug, values)
+    aug.rm('$resource/options/*')
+    values.each do |opt|
+      break if opt == :absent
+      k, v = opt.split('=')
+      if v.nil?
+        aug.clear("$resource/options/#{k}")
+      else
+        aug.set("$resource/options/#{k}", v[1..-2])  # Strip quotes
+      end
+    end
+    # Purge empty options nodes
+    aug.rm('$resource/options[count(*)=0]')
+  end
+
+  define_aug_method!(:create) do |aug, resource|
+    aug.defnode('resource', "$target/key[comment='#{resource[:name]}']", resource[:key])
+    set_options(aug, resource[:options])
+    aug.set('$resource/type', resource[:type].to_s)
+    aug.set('$resource/comment', resource[:name])
+  end
+
+  def options=(values) 
+    augopen! do |aug|
+      self.class.set_options(aug, values)
+    end
+  end
+
+  define_aug_method(:options) do |aug, resource|
+    options = aug.match('$resource/options/*')
+    if options.empty?
+      [:absent]
+    else
+      options.map do |opt|
+        label = path_label(aug, opt)
+        value = aug.get(opt)
+        if value.nil?
+          label
+        else
+          "#{label}=\"#{value}\""
+        end
+      end
+    end
+  end
+
+  def user
+    # From the ParsedFile provider
+    uid = Puppet::FileSystem.stat(target).uid
+    Etc.getpwuid(uid).name
+  end
+
+  attr_aug_accessor(:type)
+  attr_aug_accessor(:key, :label => :resource)
+end
---
+++ spec/unit/puppet/provider/ssh_authorized_key/augeas_spec.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+provider_class = Puppet::Type.type(:ssh_authorized_key).provider(:augeas)
+
+describe provider_class do
+  let (:user) {
+    # From the provider, because, really...
+    uid = Puppet::FileSystem.stat(target).uid
+    Etc.getpwuid(uid).name
+  }
+
+  before :each do
+    FileTest.stubs(:exist?).returns false
+  end
+
+  context "with empty file" do
+    let(:tmptarget) { aug_fixture("empty") }
+    let(:target) { tmptarget.path }
+
+    it "should create simple new entry" do
+      apply!(Puppet::Type.type(:ssh_authorized_key).new(
+        :name     => "nick@example.com",
+        :user     => user,
+        :type     => "ssh-rsa",
+        :key      => "AAADEADMEAT==",
+        :target   => target,
+        :provider => "augeas"
+      ))
+
+      aug_open(target, "Authorized_Keys.lns") do |aug|
+        aug.get("key").should == "AAADEADMEAT=="
+      end
+    end
+  end
+
+  context "with full file" do
+    let(:tmptarget) { aug_fixture("full") }
+    let(:target) { tmptarget.path }
+
+    it "should list instances" do
+      provider_class.stubs(:target).returns(target)
+      inst = provider_class.instances.map { |p|
+        {
+          :name   => p.get(:name),
+          :user   => p.get(:user),
+          :ensure => p.get(:ensure),
+          :type   => p.get(:type),
+          :key    => p.get(:key),
+        }
+      }
+
+      inst.size.should == 5 
+      inst[0].should == {:name=>"user@example.net", :user=>user, :ensure=>:present, :type=>"ssh-rsa", :key=>"AAAAB3NzaLiPk=="}
+      inst[1].should == {:name=>"john@example.net", :user=>user, :ensure=>:present, :type=>"ssh-rsa", :key=>"AAAAB219Q=="}
+      inst[2].should == {:name=>"example.net", :user=>user, :ensure=>:present, :type=>"ssh-dss", :key=>"AAAAC351R=="}
+      inst[4].should == {:name=>"jane@example.net", :user=>user, :ensure=>:present, :type=>"ssh-rsa", :key=>"AAAA...=="}
+    end
+
+    describe "when creating settings" do
+      it "should create new entry" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "nick@example.com",
+          :user     => user,
+          :type     => "ssh-rsa",
+          :key      => "AAADEADMEAT==",
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.get("key[.='AAADEADMEAT==']/comment").should == "nick@example.com"
+        end
+      end
+
+      it "should create new entry with options" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "sue@example.com",
+          :user     => user,
+          :type     => "ssh-rsa",
+          :key      => "ADEADMEATBB==",
+          :options  => [ "no-pty", 'command="/usr/bin/secure-shell"'],
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.match("key[comment='sue@example.com']/options/no-pty").size.should == 1
+          aug.get("key[comment='sue@example.com']/options/command").should == "/usr/bin/secure-shell"
+        end
+      end
+    end
+
+    describe "when deleting settings" do
+      it "should remove entry" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "user@example.net",
+          :ensure   => "absent",
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.match("key[comment='user@example.net']").size.should == 0
+        end
+      end
+    end
+
+    describe "when updating settings" do
+      it "should change the user's key" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "user@example.net",
+          :key      => "DEADMEAT",
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.get("key[comment='user@example.net']").should == "DEADMEAT"
+        end
+      end
+
+      it "should remove all options" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "john@example.net",
+          :options  => [:absent],
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.match("key[comment='john@example.net']/options/*").size.should == 0
+        end
+      end
+
+      it "should change the command option" do
+        apply!(Puppet::Type.type(:ssh_authorized_key).new(
+          :name     => "jane@example.net",
+          :options  => ['command="/bin/false"'],
+          :target   => target,
+          :provider => "augeas"
+        ))
+
+        aug_open(target, "Authorized_Keys.lns") do |aug|
+          aug.get("key[comment='jane@example.net']/options/command").should == "/bin/false"
+        end
+      end
+    end
+  end
+
+  context "with broken file" do
+    let(:tmptarget) { aug_fixture("broken") }
+    let(:target) { tmptarget.path }
+
+    it "should fail to load" do
+      txn = apply(Puppet::Type.type(:ssh_authorized_key).new(
+        :name     => "nick@example.com",
+        :user     => "nick",
+        :type     => "ssh-rsa",
+        :key      => "AAADEADMEAT==",
+        :target   => target,
+        :provider => "augeas"
+      ))
+
+      txn.any_failed?.should_not == nil
+      @logs.first.level.should == :err
+      @logs.first.message.include?(target).should == true
+    end
+  end
+end
