40e27dd93df2ef3b1f378d1519f2c73cd4e51a86
Jason Grammenos <jason.grammenos@mediamiser.com>
2020-05-27 14:32:37 -0400

    Add comment feature copied from augeasproviders_shellvar

--- lib/puppet/type/sshd_config.rb
+++ lib/puppet/type/sshd_config.rb
@@ -130,6 +130,10 @@ whitespace.  This is used if the `Match` block has multiple criteria.
     end
   end
 
+  newproperty(:comment) do
+    desc "Text to be stored in a comment immediately above the entry.  It will be automatically prepended with the name of the variable in order for the provider to know whether it controls the comment or not."
+  end
+
   autorequire(:file) do
     self[:target]
   end
